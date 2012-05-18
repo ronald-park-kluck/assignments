300878f98ac4526a834e520a1c1027fedddef692
ronald-park-kluck <ronaldkluck@gmail.com>
2012-05-17 22:49:27 -0400

    new js file

---
+++ mtm1526/assignment-1/js/general.js
@@ -0,0 +1,19 @@
+//JavaScript Document
+//Letter Looper
+
+var a=('');
+
+while (!x) {
+	var x = prompt('Please type a letter:');
+}
+
+console.log(x);
+
+for (var i=1; i< 10; i++){
+for (var n=1; n< i; n++){
+
+	document.write(x);
+}
+
+	document.write('</br>');
+}
