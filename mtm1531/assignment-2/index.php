c634586a736f0ec70c7d32bb8b1739b2c33211fd
ronald-park-kluck <ronaldkluck@gmail.com>
2012-05-23 08:42:26 -0400

    added labels, input fields and a button inside the form.

--- mtm1531/assignment-2/index.php
+++ mtm1531/assignment-2/index.php
@@ -14,11 +14,23 @@
 
 <body>
 	<form action="index.php" method="post">
-	
-	
-	
-	
-	
+		<label for="num1">First Number</label>
+			<input type="number" id="num1" name="num1">
+			
+			<label for="num2">Second Number</label>
+			<input type="number" id="num2" name="num2">
+			
+			<label for="func">Function</label>
+			<select id="func" name="func">
+				<option value="+">+</option>
+				<option value="-">-</option>
+				<option value="*">*</option>
+				<option value="/">/</option>
+			</select>
+			
+		<button>Calculate</button>
+		
+		<strong>$<?php echo number_format($total, 2); ?></strong>
 	</form>
 </body>
 </html>
