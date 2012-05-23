b0cf80e74c204e3493e6759456ec49c9971ca1f7
ronald-park-kluck <ronaldkluck@gmail.com>
2012-05-23 11:49:43 -0400

    linked a css file to index.php

--- mtm1531/assignment-2/index.php
+++ mtm1531/assignment-2/index.php
@@ -40,30 +40,32 @@ $total = $answer * 1.13;
 
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta charset="utf-8">
-<title>My Money Calculator</title>
-</head>
-
-<body>
-	<form action="index.php" method="post">
-		<label for="num1">First Number</label>
-			<input type="number" id="num1" name="num1">
-			
-			<label for="num2">Second Number</label>
-			<input type="number" id="num2" name="num2">
-			
-			<label for="func">Function</label>
-			<select id="func" name="func">
-				<option value="+">+</option>
-				<option value="-">-</option>
-				<option value="*">*</option>
-				<option value="/">/</option>
-			</select>
-			
-		<button>Calculate</button>
+	<head>
+		<meta charset="utf-8">
+		<title>My Money Calculator</title>
+		<link href="css/general.css" rel="stylesheet">
 		
-		<strong>$<?php echo number_format($total, 2); ?></strong>
-	</form>
-</body>
+	</head>
+	
+	<body>
+		<form action="index.php" method="post">
+			<label for="num1">First Number</label>
+				<input type="number" id="num1" name="num1">
+				
+				<label for="num2">Second Number</label>
+				<input type="number" id="num2" name="num2">
+				
+				<label for="func">Function</label>
+				<select id="func" name="func">
+					<option value="+">+</option>
+					<option value="-">-</option>
+					<option value="*">*</option>
+					<option value="/">/</option>
+				</select>
+				
+			<button>Calculate</button>
+			
+			<strong>$<?php echo number_format($total, 2); ?></strong>
+		</form>
+	</body>
 </html>
