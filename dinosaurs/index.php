43b7ef5b34b22c59b4b0fa166543e03e2adedb49
ronald-park-kluck <ronaldkluck@gmail.com>
2012-06-04 12:53:06 -0400

    created a new index.php and db.php.

---
+++ dinosaurs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Dinosaurs</title>
+	</head>
+	
+	<body>
+		
+		<dl>
+			<h2>Velociraptor</h2>
+			<dt>Loves meat</dt>
+			<dd>1</dd>
+			<dt>In Jurassic Park</dt>
+			<dd>1</dd>
+		</dl>
+		
+	</body>
+</html>
