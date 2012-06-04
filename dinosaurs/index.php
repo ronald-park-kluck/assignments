1a6cb536b23dde6c610ffa991d9eaf4fc41b3899
ronald-park-kluck <ronaldkluck@gmail.com>
2012-06-04 13:34:17 -0400

    Changed the way it gets the information from the database

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -1,3 +1,19 @@
+<?php
+
+require_once 'includes/db.php';
+
+$sql = $db->query('
+	SELECT id, dino_name, loves_meat, in_jurassic_park
+	FROM dinosaurs
+	ORDER BY dino_name ASC
+');
+
+// Display the last error created by our database
+var_dump($db->errorInfo());
+
+$results = $sql->fetchAll();
+
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -7,13 +23,15 @@
 	
 	<body>
 		
+		<?php foreach ($results as $dino) : ?>
+		<h2><?php echo $dino['dino_name']; ?></h2>
 		<dl>
-			<h2>Velociraptor</h2>
 			<dt>Loves meat</dt>
-			<dd>1</dd>
+			<dd><?php echo $dino['loves_meat']; ?></dd>
 			<dt>In Jurassic Park</dt>
-			<dd>1</dd>
+			<dd><?php echo $dino['in_jurassic_park']; ?></dd>
 		</dl>
+		<?php endforeach; ?>
 		
 	</body>
 </html>
