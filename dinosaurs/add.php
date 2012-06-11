d98ef22da6268c5c6528d27194a7b2bfc16271d9
ronald-park-kluck <ronaldkluck@gmail.com>
2012-06-11 12:43:18 -0400

    created new html for add.php

--- dinosaurs/add.php
+++ dinosaurs/add.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -6,5 +8,27 @@
 	</head>
 	
 	<body>
+		
+		<h1>Add New Dinosaur</h1>
+		
+		<form method="method" action="add.php">
+			
+			<div>
+				<label for="dino_name">Dinosaur Name</label>
+				<label id="dino_name" name="dino_name" required>
+			</div>
+			
+			<fieldset>
+				<legend>Relationship with meat</legend>
+				<input type="radio" id="love" name="loves_meat" value="1">
+				<label for="love">Loves meat</label>
+				<input type="radio" id="hate" name="loves_meat" value="0">
+				<label form="hate">Hates Meat</label>
+			</fieldset>
+			
+			<div>
+				<input type="checkbox" id="in_jurassic_park" name="in_jurassic_park">
+				<label for="in_jurassic_park">In Jurassic Park</label>
+			</div>
 	</body>
 </html>
