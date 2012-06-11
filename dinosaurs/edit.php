6eb1d659df8d50ced459e76e6fda57d323751cf9
ronald-park-kluck <ronaldkluck@gmail.com>
2012-06-11 14:49:30 -0400

    made changes so updates to dinosaurs are possible

--- dinosaurs/edit.php
+++ dinosaurs/edit.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once 'includes/db.php';
+
+$errors = array();
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $dino_name = filter_input(INPUT_POST, 'dino_name', FILTER_SANITIZE_STRING);
 $loves_meat = filter_input(INPUT_POST, 'loves_meat', FILTER_SANITIZE_NUMBER_INT);
 $in_jurassic_park = (isset($_POST['in_jurassic_park'])) ? 1 : 0;
@@ -18,12 +23,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	if (empty($errors)) {
 		// Do DB stuff
-		require_once 'includes/db.php';
 		
 		$sql = $db->prepare('
-		INSERT INTO dinosaurs (dino_name, loves_meat, in_jurassic_park)
-		VALUES (:dino_name, :loves_meat, :in_jurassic_park)
+			UPDATE dinosaurs
+			SET dino_name = :dino_name
+				,loves_meat = :loves_meat
+				,in_jurassic_park = :in_jurassic_park
+			WHERE id = :id
 		');
+		$sql->bindValue(':id', $id, PDO::PARAM_INT);
 		$sql->bindValue(':dino_name', $dino_name, PDO::PARAM_STR);
 		$sql->bindValue(':loves_meat', $loves_meat, PDO::PARAM_INT);
 		$sql->bindValue(':in_jurassic_park', $in_jurassic_park, PDO::PARAM_INT);
@@ -34,6 +42,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		header('Location: index.php');
 		exit;
 	}
+} else {
+	$sql = $db->prepare('
+		SELECT dino_name, loves_meat, in_jurassic_park
+		FROM dinosaurs
+		WHERE id = :id
+	');
+	$sql->bindValue(':id', $id, PDO::PARAM_INT);
+	$sql->execute();
+	$results = $sql->fetch();
+	
+	$dino_name = $results['dino_name'];
+	$loves_meat = $results['loves_meat'];
+	$in_jurassic_park = $results['in_jurassic_park'];
 }
 
 ?>
@@ -48,9 +69,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
 	<body>
 		
-		<h1>Add New Dinosaur</h1>
+		<h1>Edit this Dinosaur</h1>
 		
-		<form method="post" action="add.php">
+		<form method="post" action="edit.php?id=<?php echo $id; ?>">
 			
 			<div>
 				<label for="dino_name">
@@ -83,6 +104,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				<label for="in_jurassic_park">In Jurassic Park</label>
 			</div>
 			
-			<button type="submit">Add</button>
+			<button type="submit">Save</button>
 	</body>
 </html>
