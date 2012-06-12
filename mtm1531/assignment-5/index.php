cfac0d8b83eb7665f5e6913a9ec0b748b109a421
ronald-park-kluck <ronaldkluck@gmail.com>
2012-06-11 22:16:04 -0400

    created assignment-5.   included index.php, single.php, delete.php and a folde named includes with db.php inside of it.    somehow it's not working???

---
+++ mtm1531/assignment-5/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'includes/db.php';	
+
+$sql = $db->query('
+	SELECT id`, movie_title, release_date, director
+	FROM movies
+	ORDER BY movie_title ASC
+');
+
+// Display the last error created by our database
+//var_dump($db->errorInfo());
+
+$results = $sql->fetchAll();
+
+?><!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Movie Browser</title>
+	</head>
+	
+	<body>
+		
+		<?php foreach ($results as $movie) : ?>
+		<h2>
+			<a href="single.php?id=<?php echo $movie['id']; ?>">
+				<?php echo $movie['movie_title']; ?>
+			</a>
+		</h2>
+		<dl>
+			<dt>Release Date</dt>
+			<dd><?php echo $movie['release_date']; ?></dd>
+			<dt>Director</dt>
+			<dd><?php echo $movie['director']; ?></dd>
+		</dl>
+		<?php endforeach; ?>
+		
+	</body>
+</html>
---
+++ mtm1531/assignment-5/single.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+$sql = $db->prepare('
+	SELECT id, movie_title, release_date, director
+	FROM movies
+	WHERE id = :id
+
+');
+
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
+$sql->execute();
+$results = $sql->fetch();
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title><?php echo $results['movie_title']; ?></title>
+	<link href="css/general.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="movie">
+			<h2><?php echo $results['movie_title']; ?></h2>
+				<dl>
+				<dt>Release Date:</dt>
+				<dd><?php echo $results['release_date'];?></dd>
+				<dt>Director:</dt>
+				<dd><?php echo $results['director'];?></dd>
+			</dl>
+			
+			<a href="delete.php?id=<?php echo $id;?>">Delete this movie</a>
+		</div>
+		
+		<p><a href="index.php">Back to list</a></p>
+	</body>
+</html>
